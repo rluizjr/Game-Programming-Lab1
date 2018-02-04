3836d78d63f89ba75a1c72ade8110eb7a7a55167
Renato <rejr8@hotmail.com>
2018-02-03 19:32:51 -0500

    Starting lab 2 assignment

---
+++ COMP305-W2018/Assets/Scripts/ClickHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler : MonoBehaviour {
+    GameObject gameObject;
+
+	// Use this for initialization
+	void Start () {
+        gameObject = GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
